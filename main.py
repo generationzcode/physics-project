cf585dafa15ce5ca0cf9a1f76d6af486d95ce53f
Graph egg | (g)od of randomness <arjunarastogi@gmail.com>
2021-01-06 18:01:22 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+class quark():
+  def __init__(self):
+    self.type_of_quark="updownstrangecharmihavenoideawhatiamihaveidentitycrisis"
+  def __main__(self):
+    print("hey there! I'm a quark! I am cool! I am smaller than epic!")
+  
